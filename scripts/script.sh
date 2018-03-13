7fcfecf94e9e2e6bd13350635664d2da05ea5423
Rajan Middha <rajan.middha@innovaccer.com>
2018-03-13 19:20:44 +0530

    updated switch case

--- scripts/script.sh
+++ scripts/script.sh
@@ -8,16 +8,21 @@ split_with_time()
 split_with_size(){
 		echo "Splitting Video with respect to size"
 }
-read input
+
+echo " "
+echo "1) Split Video Chunks according to Time"
+echo "2) Split Video Chunks according to Size"
+echo " "
+
+read -p "Enter Input: " input
+echo " "
+
 case "$input" in
-	"1") echo "Split Video Chunks according to Time"
-		split_with_time ;;
+	1) split_with_time ;;
 
-	"2") echo "Split Video Chunks according to Size"
-		split_with_size ;;
+	2) split_with_size ;;
 
-	*) 
-		echo "Invalid Input. Exiting now"
-		exit 43 ;;
+	*) echo "Invalid Input. Exiting now"
+	   exit 43 ;;
 esac
 	
