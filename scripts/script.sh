1ee43aa18266483040f28629efac29490023a1c1
Rajan Middha <rajan.middha@innovaccer.com>
2018-03-14 22:57:23 +0530

    updated abs path

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 ##__AUTHOR: RAJAN MIDDHA__##
 
-INPUT_FILE_LOCATION="$(pwd)"
+INPUT_FILE_LOCATION="/Users/rajan/ns/sample"
 OUTPUT_FILE_LOCATION="$(pwd)/output_files"
-INPUT_FILE="sample.mkv"
+INPUT_FILE="sample.mp4"
 OUTPUT_FILE="out.mkv"
 
-OUTPUT_FILE_NAME="${OUTPUT_FILE%.*}"
-OUTPUT_FILE_EXTENSION="${OUTPUT_FILE##*.}"
+OUTPUT_FILE_NAME="${INPUT_FILE%.*}"
+OUTPUT_FILE_EXTENSION="${INPUT_FILE##*.}"
 
 mkdir -p "$OUTPUT_FILE_LOCATION"
 
 split_with_time()
 	{
-		echo "Splitting Video with respect to time"
-		SEGMENT_DURATION=""
+		echo "Splitting Video with respect to time..."
+		
+		SEGMENT_DURATION="10"
 
-		DURATION=$(ffprobe -i ${FILENAME} 2>&1 | grep Duration | awk '{print $2}' | tr -d ',')
+		DURATION=$(ffprobe -i ${INPUT_FILE_LOCATION}/${INPUT_FILE} 2>&1 | grep Duration | awk '{print $2}' | tr -d ',')
 		echo ${DURATION}
 		DURATION_TIMESTAMP=$(echo ${DURATION} | awk -F: '{ print ($1 * 3600) + ($2 * 60) + $3 }')
 		echo ${DURATION_TIMESTAMP}
 
-		ffmpeg -i ${FILENAME} -c copy -map 0 -segment_time ${SEGMENT_DURATION} -f segment sample%03d.mkv
+		ffmpeg -i ${INPUT_FILE_LOCATION}/${INPUT_FILE} -c copy -map 0 -segment_time ${SEGMENT_DURATION} -f segment ${OUTPUT_FILE_LOCATION}/${OUTPUT_FILE_NAME}%03d.${OUTPUT_FILE_EXTENSION}
 }
 
 split_with_size(){
