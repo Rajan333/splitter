7476a2fc02592581f38ac50dc305617b77494f55
Rajan Middha <rajan.middha@innovaccer.com>
2018-05-08 21:06:43 +0530

    add command line arguments

--- scripts/merger.sh
+++ scripts/merger.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 ##__AUTHOR: RAJAN MIDDHA__##
 
-INPUT_FOLDER="/Users/rajan/ns/splitter/scripts/output_files"
-OUTPUT_FOLDER="/Users/rajan/ns/splitter/scripts/output_files"
-OUTPUT_FILE_NAME="output.mp4"
+read -p "Enter Input Folder:" INPUT_FOLDER
+read -p "Enter Output Folder:" OUTPUT_FOLDER
+read -p "Enter Output Filename:" OUTPUT_FILE_NAME
+
 CURRENT_PATH=$(pwd)
 
 merge_video_segments(){
 	echo "Creating file list..."
-	touch $CURRENT_PATH/merge_list.txt
-	
+	echo " " > $CURRENT_PATH/merge_list.txt
+
 	for segment in `ls ${INPUT_FOLDER}`
 	do
 		echo "file '${INPUT_FOLDER}/${segment}'" >> ${CURRENT_PATH}/merge_list.txt
