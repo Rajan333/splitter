d70be5ffc4b3f6896e22b0f67080fe86f616f6b5
Rajan Middha <rajan.middha@innovaccer.com>
2018-05-11 13:03:03 +0530

    added command line params

--- scripts/merger.sh
+++ scripts/merger.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 ##__AUTHOR: RAJAN MIDDHA__##
 
-read -p "Enter Input Folder:" INPUT_FOLDER
-read -p "Enter Output Folder:" OUTPUT_FOLDER
-read -p "Enter Output Filename:" OUTPUT_FILE_NAME
-
 CURRENT_PATH=$(pwd)
 
 merge_video_segments(){
@@ -17,7 +13,54 @@ merge_video_segments(){
 	done
 
 	echo "Merging Segments..."
-	ffmpeg -f concat -safe 0 -i ${CURRENT_PATH}/merge_list.txt -c copy ${OUTPUT_FOLDER}/${OUTPUT_FILE_NAME}
+	ffmpeg -f concat -safe 0 -i ${CURRENT_PATH}/merge_list.txt -c copy ${OUTPUT_FOLDER}/${OUTPUT_FILENAME}
 }
 
-merge_video_segments
+
+ARGUMENTS="$#"
+if [ "$ARGUMENTS" == "0" ]; then
+	INPUT_FOLDER="/Users/rajan/ns/splitter/scripts/output_files"
+	OUTPUT_FOLDER="/Users/rajan/ns/splitter/scripts/output_files"
+	OUTPUT_FILENAME="out.mp4"
+	merge_video_segments
+
+elif [ "$ARGUMENTS" == "6" ]; then
+	while [ "$ARGUMENTS" -gt 0 ]
+	do
+		ARGUMENTS=$(($ARGUMENTS - 2))
+		key="$1"
+		echo $key
+		case $key in
+		    -i | --input_foler)
+				INPUT_FOLDER=$2
+		    	shift 2
+		    	;;
+
+		    -o | --output_folder)
+				OUTPUT_FOLDER=$2
+				shift 2
+				;;
+
+		    -f | --file)
+				OUTPUT_FILENAME=$2
+				shift 2
+				;;
+
+		    *)  echo "$1"
+				shift
+				;;
+		esac
+	done
+
+	echo INPUT_FOLDER = "${INPUT_FOLDER}"
+	echo OUTPUT_FOLDER = "${OUTPUT_FOLDER}"
+	echo OUTPUT_FILENAME = "${OUTPUT_FILENAME}"
+
+	mkdir -p ${OUTPUT_FOLDER}
+	merge_video_segments
+
+else
+	echo "Wrong Input. Exiting now."
+	exit 77
+fi
+
